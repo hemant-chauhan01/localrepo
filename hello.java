e9f9f27cb24f564a2c73a662efd4202f9c3223b7
hemant-chauhan01 <hem.chauhan1212@gmail.com>
2026-07-12 17:09:18 +0530

    add new

--- hello.java
+++ hello.java
@@ -5,4 +5,5 @@ public static void main(String[] args) {
     System.out.println(5+9);
     System.out.println(5*348);
     System.out.println("hello orient company");
+    System.out.println("updown");
 }
